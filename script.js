967efc8317a82256439d84d982eb2760b6f38d53
Vinitharam16 <vinithabegp@gmail.com>
2023-09-16 15:01:01 +0530

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function foo(){
-    var res = document.getElementById("email").value;
-    console.log(res);
+function foo(){ 
+    var first = document.getElementById("firstname").value;
+    console.log(first);
+    var Middle = document.getElementById("Middle").value;
+    console.log(Middle);
+    var lastname = document.getElementById("lastname").value;
+    console.log(lastname);
+    var email = document.getElementById("email").value;
+    console.log(email);
+    var pincode = document.getElementById("pincode").value;
+    console.log(pincode);
 }
